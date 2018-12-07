34339ac7b56ee0353c0a94a27491c4fadb256182
harry12800 <804151219@qq.com>
2018-12-08 00:07:58 +0800

    1

---
+++ src/main/java/cn/harry12800/j2se/component/rc/adapter/HeaderViewHolder.java
@@ -0,0 +1,8 @@
+package cn.harry12800.j2se.component.rc.adapter;
+
+/**
+ * Created by harry12800 on 17-5-30.
+ */
+@SuppressWarnings("serial")
+public abstract class HeaderViewHolder extends ViewHolder {
+}
